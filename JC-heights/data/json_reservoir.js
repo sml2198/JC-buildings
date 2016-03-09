47939fc1d048bc014714f6fa7bc9c689852a84d1
Sarah Levine <sml2198@users.noreply.github.com>
2016-03-08 22:06:49 -0800

    Added files via upload

---
+++ JC-heights/data/json_reservoir.js
@@ -0,0 +1,8 @@
+var json_reservoir={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                                                                  
+"features": [
+{ "type": "Feature", "properties": { "osm_id": null, "osm_way_id": "27459116", "name": "Reservoir #2", "type": null, "aeroway": null, "amenity": null, "admin_leve": null, "barrier": null, "boundary": null, "building": null, "craft": null, "geological": null, "historic": null, "land_area": null, "landuse": "reservoir", "leisure": null, "man_made": null, "military": null, "natural": null, "office": null, "place": null, "shop": null, "sport": null, "tourism": null, "other_tags": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -74.0553188, 40.7416076 ], [ -74.0531408, 40.7407216 ], [ -74.0544927, 40.7386974 ], [ -74.0569067, 40.7398843 ], [ -74.0558552, 40.7408923 ], [ -74.0553188, 40.7416076 ] ] ] } }
+]
+}
